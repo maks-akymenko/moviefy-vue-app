88320008bb4fb3dfc9a79ee2888a3ed502539f83
Maks <maks@callpage.io>
2018-10-29 16:35:20 +0100

    Made working search bar.

--- src/components/Navbar/SearchBar.vue
+++ src/components/Navbar/SearchBar.vue
@@ -1,65 +1,107 @@
 <template>
-  <div class="autocomplete">
-    <input class="input is-medium is-rounded" v-model="searchedMovie" @input="onSearch(searchedMovie)" 
-    
-    type="text"
-    placeholder="Search for movies"
-    :class="{'is-loading': loading}"
-    >
-    <ul v-if="needToOpen" class="results">
-      <li v-for="movie in results" :key="movie.id">
-        <router-link :to="movieLink(movie.id)">
-        {{ movie.title }}
+  <section>
+    <dropdown class="app-search">
+      <input
+        class="input is-medium is-rounded"
+        slot="trigger"
+        placeholder="Type movie name..."
+        ref="searchInput"
+        v-model="searchInput"
+        @input="handleSearchInput"
+      />
+      <dropdown-item
+      v-if="searchError"
+      custom class="app-search__info">
+      Ooops, something went wrong :(
+      </dropdown-item>
+      <dropdown-item
+      v-else-if="loading"
+      custom class="app-search__info">
+      We are searching for the movies...
+      </dropdown-item>
+    <dropdown-item
+      v-else-if="searchInput === ''"
+      custom class="app-search__info">
+      Type something to see results
+      </dropdown-item>
+
+    <dropdown-item
+      v-else-if="!searchResults || !searchResults.length"
+      custom class="app-search__info">
+      We could not find anything :(
+    </dropdown-item>
+    <template v-else-if="searchResults">
+      <dropdown-item
+        v-for="result in searchResults.slice(0, 10)"
+        :key="result.id"
+        class="app-search__result"
+        :title="`${result.title}`"
+      >
+        <router-link
+          :to="moviePath(result.id)"
+          class="app-search__movie-title">
+          {{ result.title }}
+         
         </router-link>
-      </li>
-    </ul>
-  </div>
+      </dropdown-item>
+    </template>
+    </dropdown>
+  </section>
 </template>
 
 <script>
+import { Dropdown, DropdownItem } from 'buefy/dist/components/dropdown'
 import to from 'await-to-js';
 
 export default {
+  components: {
+    Dropdown,
+    DropdownItem
+  },
   data () {
     return {
-      results: [],
-      needToOpen: false,
       loading: false,
-      searchedMovie: ''
+      searchInput: '',
+      searchResults: [],
+      searchError: null
     }
   },
   methods: {
-    async onSearch(query) {
-      if (this.searchedMovie.trim().length > 0) {
-        this.needToOpen = true
-        this.loading = true
-      const [error, response] = await to(this.$store.dispatch('getMovieSearchResults', query))
+    async handleSearchInput() {
+      let query = event.target.value
 
-      if (error) throw error
-      if (response) {
-        this.results = response.results
-        console.log(this.results)
-      }
-      this.loading = false
+      if (query !== '') {
+        this.loading = true
+        const [error, searchResults] = await to(
+          this.$store.dispatch('getMovieSearchResults', query)
+        )
+        this.loading = false
+        this.searchError = error
+        this.searchResults = searchResults 
+          ? searchResults.results
+          : []
+      } else {
+        this.searchError = null
+        this.searchResults = []
+        }
+      },
+    moviePath (path) {
+      return `/movies/${path}`
       }
     },
-  movieLink (id) {
-      return {
-        path: `/movies/${id}`
+    watch: {
+      $route () {
+        this.searchInput = ''
       }
-    },
+    }
   }
-}
 </script>
 
 <style lang="scss">
-  .autocomplete {
-    position: relative;
-    .results {
-      position: absolute;
-      width: 100%;
-      height: 250px;
-      border: 1px solid white;
+  .app-search {
+    &__movie-title{
+      color: #4a4a4a;
+      display: block;
     }
   }
 </style>
