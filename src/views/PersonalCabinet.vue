9950f45fa9136bc03542378b0a633a96ce504c60
maximakymenko <maks.akymenko@gmail.com>
2018-11-15 18:00:56 +0100

    Added some basic styles and markup to look kind of personal cabinet.

--- src/views/PersonalCabinet.vue
+++ src/views/PersonalCabinet.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="personal-cabinet container section">
     <h1 class="title is-size-1 has-text-centered has-text-white-ter">Welcome to your personal cabinet</h1>
-    <div class="columns is-multiline personal-cabinet__columns">
-        <div class="personal-cabinet__options">
-          <router-link to="/movies-favorite">Favorite movies</router-link>
+        <div class="personal-cabinet__favorite-movies has-background-dark">
+          <router-link
+            class="has-text-white-ter"
+            to="/movies-favorite">Favorite movies</router-link>
         </div>
-        <div class="personal-cabinet__options"><span>Search by genres</span></div>
-        <div class="personal-cabinet__options"><span>Sort movies</span></div>
-      </div>
     </div>
 </template>
 
@@ -25,11 +23,24 @@ export default {
       justify-content: space-around;
       margin: 2rem 0;
     }
-    &__options {
+    &__favorite-movies {
+      margin: 5rem auto 0 auto;
+      display: flex;
+      align-items: center;
+      border-radius: 15px;
+      box-shadow: rgba(0, 8px, 12px, 0.2);
       width: 300px;
       height: 300px;
       background-color: whitesmoke;
-      text-align: center;
+      justify-content: center;
+      transition: transform 0.33s;
+      a {
+        font-size: 2rem;
+        font-weight: bold;
+      }
+      &:hover {
+        transform: translateY(-20px);
+      }
     }
   }
 </style>
